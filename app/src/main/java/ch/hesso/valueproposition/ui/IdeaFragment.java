1e5ce78c95e4356ece0bd2c0ad09cdb8f3452494
Sarrasin <sarrasin@rfidcenter.ch>
2014-12-20 12:42:33 +0100

    Question management in idea activity

--- app/src/main/java/ch/hesso/valueproposition/ui/IdeaFragment.java
+++ app/src/main/java/ch/hesso/valueproposition/ui/IdeaFragment.java
@@ -2,20 +2,30 @@ package ch.hesso.valueproposition.ui;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import com.melnykov.fab.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ch.hesso.valueproposition.R;
 import ch.hesso.valueproposition.utils.Constants;
 
-public class IdeaFragment extends Fragment {
+public class IdeaFragment extends ListFragment {
     private EditText contentEditText;
     private int elementTypeId;
     private int currentCanvasId;
@@ -53,6 +63,35 @@ public class IdeaFragment extends Fragment {
                 getActivity().setTitle(R.string.idea_title_new);
         }
 
+        //TODO:CG Récupération des questions pour le elementTypeId donné (nice to have: limité au canvas)
+        List<String> questionList = new ArrayList<>();
+        questionList.add("Question 1");
+        questionList.add("Question 2");
+        questionList.add("Question 3");
+        questionList.add("Question 4");
+        questionList.add("Question 5");
+
+        setListAdapter(new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, questionList));
+
+        FloatingActionButton fab = (FloatingActionButton) rootView.findViewById(R.id.fab_add);
+        fab.attachToListView((ListView) rootView.findViewById(android.R.id.list));
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText input = new EditText(getActivity());
+
+                new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.idea_question_title_new)
+                    .setView(input)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            String question = input.getText().toString();
+                            //TODO:CG Ajouter question dans BD
+                        }
+                    }).setNegativeButton(android.R.string.cancel, null).show();
+            }
+        });
+
         return rootView;
     }
 
@@ -65,9 +104,6 @@ public class IdeaFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_add_question:
-                //TODO
-                return true;
             case R.id.action_delete:
                 new AlertDialog.Builder(getActivity())
                     .setMessage(R.string.idea_delete_confirmation)
@@ -77,21 +113,44 @@ public class IdeaFragment extends Fragment {
                             getActivity().finish();
                         }
                     })
-                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // User cancelled the dialog
-                        }
-                    }).create().show();
+                    .setNegativeButton(android.R.string.no, null).create().show();
                 return true;
             case R.id.action_save:
-                saveForm();
+                saveIdea();
                 return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void saveForm() {
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        /*
+        //TODO:CG Gérer la récupération de l'id depuis la position
+        //Cursor c = (Cursor) getListAdapter().getItem(position);
+        //String url = c.getString(c.getColumnIndex("_id"));
+        */
+        final int questionId = 1;
+        final EditText input = new EditText(getActivity());
+        input.setText("toto"); //TODO:CG Remplacer
+        input.setSelection(input.getText().length());
+
+        new AlertDialog.Builder(getActivity())
+            .setTitle(R.string.idea_question_title_edit)
+            .setView(input)
+            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String question = input.getText().toString();
+                    //TODO:CG Màj question d'après son questionId et le string question
+                }
+            }).setNegativeButton(R.string.idea_question_edit_delete, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    //TODO:CG Supprimer question d'après son questionId
+                }
+        }).show();
+    }
+
+    private void saveIdea() {
         String content = contentEditText.getText().toString();
 
         //TODO:CG (Create/Update content)
