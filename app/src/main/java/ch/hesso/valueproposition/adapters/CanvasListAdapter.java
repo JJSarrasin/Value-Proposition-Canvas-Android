56d8acc7563b03f6911f736c3b7220981c36bf20
gap <gapanyc@gmail.com>
2015-01-09 15:57:13 +0100

    added fake questions in db

--- app/src/main/java/ch/hesso/valueproposition/adapters/CanvasListAdapter.java
+++ app/src/main/java/ch/hesso/valueproposition/adapters/CanvasListAdapter.java
@@ -2,49 +2,40 @@ package ch.hesso.valueproposition.adapters;
 
 import android.content.Context;
 import android.content.Intent;
-import android.view.LayoutInflater;
+import android.database.Cursor;
+import android.net.Uri;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.SimpleAdapter;
-
-import java.util.List;
-import java.util.Map;
+import android.widget.ResourceCursorAdapter;
+import android.widget.TextView;
 
 import ch.hesso.valueproposition.R;
+import ch.hesso.valueproposition.db.DbObjects;
 import ch.hesso.valueproposition.ui.CanvasActivity;
-import ch.hesso.valueproposition.utils.Constants;
 
-public class CanvasListAdapter extends SimpleAdapter {
-    private final Context context;
-    private LayoutInflater	mInflater;
-    private List<Map<String, String>> data;
-    private int resource;
+public class CanvasListAdapter extends ResourceCursorAdapter {
 
-    public CanvasListAdapter(Context context, List<Map<String, String>> data, int resource, String[] from, int[] to) {
-        super(context, data, resource, from, to);
-        this.resource = resource;
-        this.context = context;
-        this.data = data;
-        mInflater = LayoutInflater.from(context);
+    public CanvasListAdapter(Context context, int layout, Cursor cursor) {
+        super(context, layout, cursor, ResourceCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
     }
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = mInflater.inflate(resource, parent, false);
+    public void bindView(View view, final Context context, final Cursor cursor) {
+        TextView titleTextView = (TextView) view.findViewById(R.id.home_card_element_title);
+        titleTextView.setText(cursor.getString(cursor.getColumnIndex(DbObjects.Canvas.COL_TITLE)));
 
-            ImageButton editButton = (ImageButton) convertView.findViewById(R.id.home_card_element_edit);
-            editButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(context, CanvasActivity.class);
-                    intent.putExtra(Constants.EXTRA_CANVAS_ID, Integer.parseInt(data.get(position).get("ID")));
-                    context.startActivity(intent);
-                }
-            });
-        }
+        TextView descTextView = (TextView) view.findViewById(R.id.home_card_element_description);
+        descTextView.setText(cursor.getString(cursor.getColumnIndex(DbObjects.Canvas.COL_DESC)));
 
-        return super.getView(position, convertView, parent);
+        ImageButton editBtn = (ImageButton) view.findViewById(R.id.home_card_element_edit);
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, CanvasActivity.class);
+                intent.setData(Uri.withAppendedPath(DbObjects.Canvas.CONTENT_URI, cursor.getString(cursor.getColumnIndex(DbObjects.Canvas._ID))));
+                context.startActivity(intent);
+            }
+        });
     }
+
 }
